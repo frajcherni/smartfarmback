43ef76e4f7a28eea135fb8dd96c506fc18e7d834
Cherni Fraj <frajcherni81@gmail.com>
2026-01-31 10:03:21 +0100

    fixe

--- src/test/java/com/farm/demo/SmartFarmApplicationTests.java
+++ src/test/java/com/farm/demo/SmartFarmApplicationTests.java
@@ -3,11 +3,13 @@ package com.farm.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.farm.prj.SmartFarmApplication;
+
+// ✅ Add the main application class explicitly
+@SpringBootTest(classes = SmartFarmApplication.class)
 class SmartFarmApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+    
+    @Test
+    void contextLoads() {
+    }
 }
